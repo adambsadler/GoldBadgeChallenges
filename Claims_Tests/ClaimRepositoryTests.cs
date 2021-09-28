0d6523416c45011959e095950b69b6940726410c
Adam Sadler <adambsadler@gmail.com>
2021-09-28 11:39:50 -0400

    Claims test method added

--- Claims_Tests/ClaimRepositoryTests.cs
+++ Claims_Tests/ClaimRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Claims_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Claims_Tests
@@ -6,9 +7,22 @@ namespace Claims_Tests
     [TestClass]
     public class ClaimRepositoryTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ClaimRepository _repo;
+        private Claim _claim;
+
+        [TestInitialize]
+        public void Arrange()
         {
+            _repo = new ClaimRepository();
+            _claim = new Claim(1, "Car", "Broken windshield", 527.89m, new DateTime(2021,5,16), new DateTime(2021,5,28), true);
+        }
+        [TestMethod]
+        public void CreateNewClaim_ShouldGetNotNull()
+        {
+            _repo.CreateNewClaim(_claim);
+            Claim claimInQueue = _repo.SeeNextClaim();
+
+            Assert.IsNotNull(claimInQueue);
         }
     }
 }
